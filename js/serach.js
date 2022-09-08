b3b960c371f60aeec46918ed4818cca8cd82fbce
Kai Barry <88063818+Kai-Barry@users.noreply.github.com>
2022-09-08 15:19:42 +1000

    javascript basic addidtion

---
+++ js/serach.js
@@ -0,0 +1,35 @@
+// function inputSerach() {
+//     // var input, filter, ul, li, a, i, txtValue;
+//     // const badWordsStr = FileReader.readAsText("Badwords/badWords.txt")
+//     // input = document.getElementById(input)
+//     // const badWords = badWordsStr.split(",")
+//     document.getElementById("test").innerHTML = "test" 
+// }
+
+// function handle(e){
+//     if(e.key === "Enter"){
+//         alert("Enter was just pressed.");
+//     }
+
+//     return false;
+// }
+
+// function event(e) {
+//     var input = document.getElementById("myInput");
+
+//     // Execute a function when the user releases a key on the keyboard
+//     input.addEventListener("keyup", function(event) {
+//     // Number 13 is the "Enter" key on the keyboard
+//     if (event.keyCode === 13) {
+//         // Cancel the default action, if needed
+//         event.preventDefault();
+//         // Trigger the button element with a click
+//         document.getElementById("myBtn").click();
+//     }
+//     });
+// }
+
+// function myFunction(event) {
+//     var x = event.key;
+//     document.getElementById("demo").innerHTML = "The pressed key was: " + x;
+//   }
