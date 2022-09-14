b826f71c10db423dd2f5e26b3e93063b75f20d4a
Eco9898 <eco9898@live.com>
2022-09-14 14:59:11 +1000

    Update genPage.php

--- genPage.php
+++ genPage.php
@@ -1,12 +1,9 @@
 <?php
 	$page = $_GET["page"];
-	$files = glob($dir . 'pages/*.data');/**/
-    $file = array_rand($files);
-
 	$command = escapeshellcmd('python3.10 /var/www/html/genPages.py');
 	$output = shell_exec($command);
-	/*echo $output;*/)
+	/*echo $output;*/
 
-    header('Location: https://deco3801-dinosandcometsequaldeath.uqcloud.net/info.php?page=' . substr($files[$file], 6, strlen($file)-6));
+    header('Location: https://deco3801-dinosandcometsequaldeath.uqcloud.net/info.php?page=' . $page);
 	exit();
 ?>
