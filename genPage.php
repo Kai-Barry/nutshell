f4cababda91f2c2a920d9988945bee32698245e0
Eco9898 <eco9898@live.com>
2022-09-21 15:16:35 +1000

    Fixed page generation

--- genPage.php
+++ genPage.php
@@ -1,11 +1,12 @@
 <?php
 	$page = $_GET["page"];
-	$command = escapeshellcmd('/var/www/html/genPage.sh \"' . $page .  "\" 2>&1 &");
+	$command = escapeshellcmd('/var/www/html/genPage.sh "' . $page .  '" 2>&1 &');
 	$output = shell_exec($command);
 	echo $output;
-	if ($output=="Success") {
+	if (strpos($output, "Success") !== false) {
 		//echo "passed";
 		header('Location: https://deco3801-dinosandcometsequaldeath.uqcloud.net/info.php?page=' . $page);
 	}
 	exit();
 ?>
+
