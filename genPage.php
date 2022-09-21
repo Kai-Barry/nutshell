7605bed7a5450fe2b65b82d84b54577fbc7051e4
Eco9898 <eco9898@live.com>
2022-09-21 14:17:58 +1000

    Fix Error detection in generating responses

--- genPage.php
+++ genPage.php
@@ -3,7 +3,7 @@
 	$command = escapeshellcmd('/var/www/html/genPage.sh ' . $page);
 	$output = shell_exec($command .  " 2>&1 &");
 	echo $output;
-	if ($output="Success") {
+	if ($output=="Success") {
 		//echo "passed";
 		header('Location: https://deco3801-dinosandcometsequaldeath.uqcloud.net/info.php?page=' . $page);
 	}
