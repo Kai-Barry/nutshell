838e5a5162e797dbfe76912243d1fb29f7f5a0d4
Eco9898 <eco9898@live.com>
2022-10-05 14:15:59 +1000

    Create template.php

---
+++ template.php
@@ -0,0 +1,13 @@
+<?php
+	$headerFile = file_get_contents("./pages/header.html", FILE_USE_INCLUDE_PATH);
+	$footerFile = file_get_contents("./pages/footer.html", FILE_USE_INCLUDE_PATH);
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+    <!--insert header code here-->
+	<title><!--Page title the php will append the site name after--><?php
+		echo $headerFile; ?>
+    <!--insert body code here-->
+    </body>
+</html>
