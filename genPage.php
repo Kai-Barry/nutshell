1957182a2e1482916481b280bb3ce49cf6ce7e10
Eco9898 <eco9898@live.com>
2022-10-05 13:04:11 +1000

    genPage fowards to info.php when the page exists

--- genPage.php
+++ genPage.php
@@ -3,7 +3,7 @@
 	$command = escapeshellcmd('/var/www/html/genPage.sh "' . $page .  '" 2>&1 &');
 	$output = shell_exec($command);
 	echo $output;
-	if (strpos($output, "Success") !== false) {
+	if (strpos($output, "Success") !== false || strpos($output, "Page exists") !== false) {
 		//echo "passed";
 		header('Location: https://deco3801-dinosandcometsequaldeath.uqcloud.net/info.php?page=' . $page);
 	}
