965f0e18fe82f2c7ae3ea098f9c61d0b23c1fda5
Kai Barry <88063818+Kai-Barry@users.noreply.github.com>
2022-10-05 14:31:23 +1000

    Create oh no page

---
+++ wrong.php
@@ -0,0 +1,34 @@
+<?php
+	$headerFile = file_get_contents("./pages/header.html", FILE_USE_INCLUDE_PATH);
+	$footerFile = file_get_contents("./pages/footer.html", FILE_USE_INCLUDE_PATH);
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+	<title>Oh No!<?php
+		echo $headerFile; ?>
+        <div class="one">
+            <div class="oh-no-page">
+                <div class="no-page">
+                    <div class="no-column">
+                        <div class="no-title">
+                            <div>
+                                <h1>Oh No!</h1>
+                            </div>
+                        </div>
+                        <img id="squirrel" src="Images/Logo 01- 600 x 600 px.png" alt="Navigation logo">
+                    </div>
+                    <div class="no-column">
+                        <div class="no-box">
+                            <p>
+                                It looks like we’ve found a tough nut and can’t break into it. Maybe try to look for something else?
+                            </p>
+                        </div>
+                        <img id="acorn" src="Images/Logo 02- 600 x 600 px.png">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php echo $footerFile;?>
+    </body>
+</html>
