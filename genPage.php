e3b41ec50fc9a9b528978ee5d805f15c0906a13b
eco9898 <ecoprograms01@gmail.com>
2022-10-09 17:06:45 +1000

    Update genPage.php

--- genPage.php
+++ genPage.php
@@ -6,11 +6,11 @@
 	if (strpos($output, "Success") !== false || strpos($output, "Page exists") !== false) {
 		//echo "passed";
 		$page = explode(": ", $output)[1];
-		if (strpos($output, "Success") !== false) {
+		/*if (strpos($output, "Success") !== false) {
 			$files = json_decode(file_get_contents('dict.json'), true);
 			$files["pages/" . $page  . ".data"] = 0;
 			file_put_contents("dict.json",json_encode($files));
-		}
+		}*/
 		header('Location: https://deco3801-dinosandcometsequaldeath.uqcloud.net/Article.php?page=' . $page);
 	}
 	exit();
