f6e7d5eb49369e5ca29cdeb5a655c157c3af2d0f
FinnRobertson15 <110511080+FinnRobertson15@users.noreply.github.com>
2022-10-09 14:28:36 +1000

    Update Article.php

--- Article.php
+++ Article.php
@@ -108,7 +108,7 @@ $photos = Unsplash\Search::photos($search, $page, $per_page, $orientation);
                         <p>Intro "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."</p>
                     </div>
                     <div class="artHeadimg">
-                        <img src="images/placeholder.jpg">
+                        <img src=<?php echo photos[0];?>>
                     </div>
                 </div>
                 <br>
