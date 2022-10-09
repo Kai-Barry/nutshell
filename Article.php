2165c4ad19eed2383f485b6bb13f24eedbc14e7b
eco9898 <ecoprograms01@gmail.com>
2022-10-09 16:25:14 +1000

    Added loading data to article.php

--- Article.php
+++ Article.php
@@ -70,10 +70,28 @@ for ($x = 0; $x <= 5; $x++) {
 		//echo "\nlooped";
 }
 
+$headings = [];
+$i = 0;
+//Isolate headings
+foreach ($paras as $para) {
+    $headings[$i] = explode("\n",$para)[0]
+    $i++;
+}
+
+//Isolate paras
+$paragraphs = [];
+$i = 0;
+//Isolate headings
+foreach ($paras as $para) {
+    $paragraphs[$i] = explode("\n",$para)[1]
+    $i++;
+}
+
 $search = "dinosaur";
 $page = 1;
 $per_page = 5;
 $orientation = "landscape";
+
 // $testimage = 'https://images.unsplash.com/photo-1606856110002-d0991ce78250?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80';
 // $photos = Unsplash\Search::photos($search, $page, $per_page, $orientation);
 ?>
@@ -87,16 +105,16 @@ $orientation = "landscape";
         <div class="jumptomenu">
             <div class="jumpto">
                 <div class="sub1">
-                    <a href="#Sub1" id="subOne">Extinction</a>
+                    <a href="#Sub1" id="subOne"><?php echo $headings[1];?></a>
                 </div>
                 <div class="sub2">
-                    <a href="#Sub2" id="subTwo">Life Cycle</a>
+                    <a href="#Sub2" id="subTwo"><?php echo $headings[2];?></a>
                 </div>
                 <div class="sub3">
-                    <a href="#Sub3" id="subThree">Species</a>
+                    <a href="#Sub3" id="subThree"><?php echo $headings[3];?></a>
                 </div>
                 <div class="sub4">
-                    <a href="#Sub4" id="subFour">Other1</a>
+                    <a href="#Sub4" id="subFour"><?php echo $headings[4];?></a>
                 </div>
             </div>
         </div>
@@ -105,7 +123,7 @@ $orientation = "landscape";
                 <div class="artHead">
                     <div class="artHeadtxt">
                         <h1><?php echo $header;?></h1>
-                        <p>Intro "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."</p>
+                        <p><?php echo $paragraphs[0];?></p>
                     </div>
                     <div class="artHeadimg">
                         <img src='images/placeholder.jpg'>
@@ -120,15 +138,7 @@ $orientation = "landscape";
                     </div>
                     <div class="sub1txt">
                         <h3 id="Sub1">Sub1</h3>
-                        <p>-Basketball was invented in 1891 by Dr. James Naismith. 
-                        -He was a physical education teacher at a YMCA in Springfield, Massachusetts. 
-                        -He came up with the game to keep his students occupied during the winter. 
-                        -He nailed a peach basket to the gymnasium wall and the game was born. 
-                        -The first game was played with a soccer ball and two teams of nine players. 
-                        -The game was originally called “Naismith’s game” or “indoor baseball”. 
-                        -Basketball quickly became popular and by 1893, there were already over 100 YMCA’s playing the game. 
-                        -In 1896, the first professional basketball league was formed and in 1898, the first official college game was played. 
-                        -The game has continued to grow in popularity and is now one of the most popular sports in the world.</p>
+                        <p><?php echo $paragraphs[1];?></p>
                     </div>
                 </div>
                 <br>
@@ -137,7 +147,7 @@ $orientation = "landscape";
                 <div class="artSub2">
                     <div class="sub2txt">
                         <h3 id="Sub2">Sub2</h3>
-                        <p>Sub2</p>
+                        <p><?php echo $paragraphs[2];?></p>
                     </div>
                     <div class="sub2img">
                         <img src="images/placeholder.jpg">
@@ -151,7 +161,7 @@ $orientation = "landscape";
                         <img src="images/placeholder.jpg">
                     </div>
                     <div class="sub3txt">
-                        <h3 id="Sub3">sub3</h3>
+                        <h3 id="Sub3"><?php echo $paragraphs[3];?></h3>
                         <p>sub3</p>
                     </div>
                 </div>
@@ -160,7 +170,7 @@ $orientation = "landscape";
                 <br>
                 <div class="artSub4">
                     <div class="sub4txt">
-                        <h3 id="Sub4">sub4</h3>
+                        <h3 id="Sub4"><?php echo $paragraphs[4];?></h3>
                         <p>sub4</p>
                     </div>
                     <div class="sub4img">
