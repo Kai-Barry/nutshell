a17245c8ca6b13414ef9c3c90a1cf21b6d287c8a
eco9898 <ecoprograms01@gmail.com>
2022-10-10 11:14:39 +1000

    Update rerunGenPages.py

--- rerunGenPages.py
+++ rerunGenPages.py
@@ -2,4 +2,4 @@ import os
 for filename in os.listdir("pages"):
 	if ".data" in filename:
 		os.remove("pages/" + filename)
-		os.system("./genPage.sh " + filename.split(".data")[0])
+		os.system("./genPage.sh " + filename.split(".data")[0].replace(" ", "\\ "))
