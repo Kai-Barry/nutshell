13dd8511e12be994d4e47ce8af0219c8144201e3
eco9898 <ecoprograms01@gmail.com>
2022-10-10 10:40:38 +1000

    Update genPage.sh

--- genPage.sh
+++ genPage.sh
@@ -1,2 +1,12 @@
-export OPENAI_API_KEY=Key
+COUNTER=$((RANDOM%2))
+case $COUNTER in
+        0) export OPENAI_API_KEY=1;;
+        1) export OPENAI_API_KEY=1;;
+esac
+COUNTER=$((RANDOM%3))
+case $COUNTER in
+        0) export SERPAPI_KEY=1;;
+        1) export SERPAPI_KEY=1;;
+        2) export SERPAPI_KEY=1;;
+esac
 python3.10 genPages.py "$1"
