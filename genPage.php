cfad4a0fe2a6f86c4342977213c82e2cbb62fb2d
eco9898 <ecoprograms01@gmail.com>
2022-10-10 11:38:09 +1000

    allow multiple pages to be generated in one command

--- genPage.php
+++ genPage.php
@@ -1,21 +1,21 @@
 <?php
-	$page = $_GET["page"];
-	$command = escapeshellcmd('/var/www/html/genPage.sh "' . $page .  '" 2>&1 &');
-	$output = shell_exec($command);
-	echo $output;
-	if (strpos($output, "Success") !== false || strpos($output, "Page exists") !== false) {
-		//echo "passed";
-		$page = explode(": ", $output)[1];
-		/*if (strpos($output, "Success") !== false) {
-			$files = json_decode(file_get_contents('dict.json'), true);
-			$files["pages/" . $page  . ".data"] = 0;
-			file_put_contents("dict.json",json_encode($files));
-		}*/
-		header('Location: https://deco3801-dinosandcometsequaldeath.uqcloud.net/Article.php?page=' . $page);
-	}
-	else {
-		header('Location: https://deco3801-dinosandcometsequaldeath.uqcloud.net/OhNoPage.php');
-	}
-	exit();
+        $page = $_GET["page"];
+        $command = escapeshellcmd('/var/www/html/genPage.sh "' . $page . '"');
+        //echo $command;
+        $output = shell_exec($command);
+        echo $output;
+        if (strpos($output, "Success") !== false || strpos($output, "Page exists") !== false) {
+                //echo "passed";
+                $page = explode(": ", $output)[1];
+                /*if (strpos($output, "Success") !== false) {
+                        $files = json_decode(file_get_contents('dict.json'), true);
+                        $files["pages/" . $page  . ".data"] = 0;
+                        file_put_contents("dict.json",json_encode($files));
+                }*/
+                header('Location: https://deco3801-dinosandcometsequaldeath.uqcloud.net/Article.php?page=' . $page);
+        }
+        /*else {
+                header('Location: https://deco3801-dinosandcometsequaldeath.uqcloud.net/OhNoPage.php');
+        }*/
+        exit();
 ?>
-
--- genPage.sh
+++ genPage.sh
@@ -1,12 +1,17 @@
-COUNTER=$((RANDOM%2))
-case $COUNTER in
-        0) export OPENAI_API_KEY=1;;
-        1) export OPENAI_API_KEY=1;;
-esac
-COUNTER=$((RANDOM%3))
-case $COUNTER in
-        0) export SERPAPI_KEY=1;;
-        1) export SERPAPI_KEY=1;;
-        2) export SERPAPI_KEY=1;;
-esac
-python3.10 genPages.py "$1"
+for var in "${@}"
+do
+        COUNTER=$(($(od -A n -t d -N 1 /dev/urandom)%4))
+        case $COUNTER in
+                0) export OPENAI_API_KEY=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 && echo 1;;
+                1) export OPENAI_API_KEY=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 && echo 2;;
+                2) export OPENAI_API_KEY=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 && echo 3;;
+                3) export OPENAI_API_KEY=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 && echo 4;;
+        esac
+        COUNTER=$(($(od -A n -t d -N 1 /dev/urandom)%3))
+        case $COUNTER in
+                0) export SERPAPI_KEY=a1f0b96d76a0809f86c5b329dedda1b8b11869c0ac148fd67f085fa48861c021 && echo 1;;
+                1) export SERPAPI_KEY=2f3b3a177216bcf79ad515111a7c60a8db9971bfe71e670a31f5614923a0bfae && echo 2;;
+                2) export SERPAPI_KEY=51db6ed21e1bdb577f0d4f696df60d26fdd35a595031daa5516b9acffe62b290 && echo 3;;
+        esac
+        python3.10 genPages.py $var
+done
