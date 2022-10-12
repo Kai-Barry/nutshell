01796d37bdcc821e2f453dbcd4b45f589c32278f
Eco9898 <eco9898@live.com>
2022-10-12 13:08:32 +1000

    Update genImages.py

--- genImages.py
+++ genImages.py
@@ -7,9 +7,7 @@ def createImages(topic):
 	params = {
 		"q": topic,
 		"tbm": "isch",
-		"api_key": "a1f0b96d76a0809f86c5b329dedda1b8b11869c0ac148fd67f085fa48861c021",
-		# "api_key": "2f3b3a177216bcf79ad515111a7c60a8db9971bfe71e670a31f5614923a0bfae",
-		# "api_key": "51db6ed21e1bdb577f0d4f696df60d26fdd35a595031daa5516b9acffe62b290",
+		"api_key": "1",
 		"safe": "active"
 	}
 
