91d59186e5b5f28f5d691263cb3ac3aeca1080bd
FinnRobertson15 <110511080+FinnRobertson15@users.noreply.github.com>
2022-10-19 00:27:14 +1000

    Update index.php

--- index.php
+++ index.php
@@ -44,6 +44,12 @@
             if (element.value.includes("\n") || element.value.includes("\r")) {
                 element.value = element.value.replace(/[\r\n]/gm, '');
                 if (element.value.length > 0) {
+                    const para = document.createElement("p");
+                    const node = document.createTextNode("Loading");
+                    para.appendChild(node);
+                    const element = document.getElementById("one");
+                    element.appendChild(para);
+                    
                     window.location.href = "/genPage.php?page=" + element.value;
                 }
             }
